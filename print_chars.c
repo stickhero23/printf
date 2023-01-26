94c9d8a05cf190d25bfcd2dbab572f06baf34bda
stickhero23 <stickhero23@gmail.com>
2023-01-26 03:44:50 +0000

    adding print_chars.c

--- print_chars.c
+++ print_chars.c
@@ -1,107 +1,13 @@
-#include "main.h"
-#include <stdlib.h>
+#include "holberton.h"
+
 /**
- * print_char - prints character
- * @c: char to print
- * Return: always 1
+ * _write_char - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int print_char(va_list c)
+int _write_char(char c)
 {
-	char ch = (char)va_arg(c, int);
-
-	_putchar(ch);
-	return (1);
-}
-/**
- * print_str - prints a string
- * @s: string to print
- * Return: a number of chars
- */
-int print_str(va_list s)
-{
-	int count;
-	char *str = va_arg(s, char *);
-
-	if (str == NULL)
-		str = "(null)";
-	for (count = 0; str[count]; count++)
-	{
-		_putchar(str[count]);
-	}
-	return (count);
-}
-/**
- * prnt_hex - prints a char's ascii in uppercase
- * @c: char to print
- * Return: Always 2, number of chars printed.
- */
-static int prnt_hex(char c)
-{
-	int count;
-	char diff = 'A' - ':';
-	char j[2];
-
-	j[0] = c / 16;
-	j[1] = c % 16;
-	for (count = 0; count < 2; count++)
-	{
-		if (j[count] >= 10)
-			_putchar('0' + diff + j[count]);
-		else
-			_putchar('0' + j[count]);
-	}
-	return (count);
-}
-/**
- * print_S - prints a string and ascii value of nonprintable character.
- * @S: string to print
- * Return: a number of chars printed
- */
-int print_S(va_list S)
-{
-	unsigned int i;
-	int count = 0;
-	char *str = va_arg(S, char *);
-
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] < 32 || str[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			count += 2;
-			count += prnt_hex(str[i]);
-		}
-		else
-		{
-			_putchar(str[i]);
-			count++;
-		}
-
-	}
-	return (count);
-}
-/**
- * print_rev - reverse print a string
- * @r: a string to print in reverse.
- * Return: number of chars printed
- */
-int print_rev(va_list r)
-{
-	char *str;
-	int i, count = 0;
-
-	str = va_arg(r, char *);
-	if (str == NULL)
-		str = ")llun(";
-	for (i = 0; str[i]; i++)
-		;
-	for (i -= 1; i >= 0; i--)
-	{
-		_putchar(str[i]);
-		count++;
-	}
-	return (count);
+	return (write(1, &c, 1));
 }
